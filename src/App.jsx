7ab04e696cbac427c77a3d3f6ae435cb398c139f
Saheb Dhara <sahebdhara961@gmail.com>
2023-09-22 09:59:45 +0530

    Cart open and close

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Fragment } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 function App() {
+  const [isClicked, setIsClicked] = useState(false); 
+
+  const ShowCartHandeler = () =>{
+    setIsClicked(true)
+  }
+  const hideCartHandeler = () =>{
+    setIsClicked(false)
+  }
   return (
     <Fragment>
-      <Cart/>
-      <Header />
+      {isClicked && <Cart onClose = {hideCartHandeler}/>}
+      <Header noShowHanderler = {ShowCartHandeler} />
       <main>
         <Meals />
       </main>
